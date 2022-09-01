1ac49d65c88b6e802c67f72130985a7ee738b2c0
skbtomek <tskubisz@objectivity.co.uk>
2022-09-01 12:12:28 +0200

    Example update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Task, ViewMode, Gantt } from "gantt-task-react";
+import { Task, ViewMode, Gantt } from "objectivity-gantt-task-react";
 import { ViewSwitcher } from "./components/view-switcher";
 import { getStartEndDateForProject, initTasks } from "./helper";
-import "gantt-task-react/dist/index.css";
+import "objectivity-gantt-task-react/dist/index.css";
 
 // Init
 const App = () => {
