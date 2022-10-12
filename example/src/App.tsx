63eeab54a84255673548504a0e31f317fb75f027
skbtomek <tskubisz@objectivity.co.uk>
2022-10-12 13:19:55 +0200

    fix: revert gantt visibility check

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,29 +3,27 @@ import { Task, ViewMode, Gantt } from "objectivity-gantt-task-react";
 import { ViewSwitcher } from "./components/view-switcher";
 import { initTasks } from "./helper";
 import "objectivity-gantt-task-react/dist/index.css";
-import { TaskListHeader } from "./components/TaskListHeader";
-import { TaskListTable } from "./components/TaskListTable";
-import { TooltipContent } from "./components/CustomTooltip";
-import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 // Init
 const App = () => {
-  const [view, setView] = React.useState<ViewMode>(ViewMode.Year);
+  const [view, setView] = React.useState<ViewMode>(ViewMode.Day);
   const [tasks, setTasks] = React.useState<Task[]>(initTasks());
   const [isChecked, setIsChecked] = React.useState(true);
-
-  const fullScreenHandle = useFullScreenHandle();
   let columnWidth = 65;
   if (view === ViewMode.Year) {
-    columnWidth = 400;
+    columnWidth = 350;
   } else if (view === ViewMode.Month) {
     columnWidth = 300;
   } else if (view === ViewMode.Week) {
     columnWidth = 250;
-  }
+  } 
 
   const handleClick = (task: Task) => {
     console.log("On Click event Id:" + task.id);
+  };
+
+  const handleSelect = (task: Task, isSelected: boolean) => {
+    console.log(task.name + " has " + (isSelected ? "selected" : "unselected"));
   };
 
   const handleExpanderClick = (task: Task) => {
@@ -33,37 +31,36 @@ const App = () => {
     console.log("On expander click Id:" + task.id);
   };
 
-  if (tasks && tasks.length > 0) {
-  
-    return (
-      <FullScreen handle={fullScreenHandle}>;
-        <ViewSwitcher
-          onViewModeChange={viewMode => setView(viewMode)}
-          onViewListChange={setIsChecked}
-          isChecked={isChecked}
-        />
-        <h3>Gantt With Unlimited Height</h3>
-        <div>
-          <Gantt
-            tasks={tasks}
-            viewMode={view}
-            onClick={handleClick}
-            onExpanderClick={handleExpanderClick}
-            listCellWidth="35%"
-            preStepsCount={1}
-            barCornerRadius={0}
-            columnWidth={columnWidth}
-            TooltipContent={TooltipContent}
-            TaskListHeader={TaskListHeader}
-            TaskListTable={TaskListTable}
-            todayColor="#FEF9E9"
-          />
-        </div>
-      </FullScreen>
-    );
-  }
-
-  return <div>No data to display</div>;
+  return (
+    <div className="Wrapper">
+      <ViewSwitcher
+        onViewModeChange={viewMode => setView(viewMode)}
+        onViewListChange={setIsChecked}
+        isChecked={isChecked}
+      />
+      <h3>Gantt With Unlimited Height</h3>
+      <Gantt
+        tasks={tasks}
+        viewMode={view}        
+        onClick={handleClick}
+        onSelect={handleSelect}
+        onExpanderClick={handleExpanderClick}
+        listCellWidth={isChecked ? "155px" : ""}
+        columnWidth={columnWidth}
+      />
+      <h3>Gantt With Limited Height</h3>
+      <Gantt
+        tasks={tasks}
+        viewMode={view}    
+        onClick={handleClick}
+        onSelect={handleSelect}
+        onExpanderClick={handleExpanderClick}
+        listCellWidth={isChecked ? "155px" : ""}
+        ganttHeight={300}
+        columnWidth={columnWidth}
+      />
+    </div>
+  );
 };
 
 export default App;
--- example/src/helper.tsx
+++ example/src/helper.tsx
@@ -1,241 +1,128 @@
+import { startOfDay } from "date-fns";
+import { endOfDay } from "date-fns/esm";
 import { Task } from "../../dist/types/public-types";
 
 export function initTasks() {
- 
+  const now = new Date();
+  
   const tasks: Task[] = [
     {
-      "id": "44754521296994505",
-      "name": "Ullamco est sunt anim dolor nostrud labore. Est tempor id ullamco sit fugiat proident magna est dolor.Tempor est tempor sint fugiatś",
-      "additionalInfo": "Ad ullamco duis ipsum Lorem.",
-      "tooltip": "Occaecat commodo eu dolor cillum sit aliqua exercitation.Dolore sit duis excepteur dolore irure",
-      "start": new Date("2022-08-24T22:00:00.000Z"),
-      "end": new Date("2022-09-22T21:59:59.999Z"),
-      "type": "project",
-      "hideChildren": true,
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#F8D87D",
-        "backgroundSelectedColor": "#F4C53C",
-        "progressColor": "#F8D87D",
-        "progressSelectedColor": "#F4C53C"
+      start: new Date(now.getFullYear(), now.getMonth(), 1),
+      end: new Date(now.getFullYear(), now.getMonth(), 15),
+      name: "Some Project sfdf Some Project sfdf Some Project sfdf ",
+      additionalInfo: "some additional info some additional info some additional info ",
+      id: "ProjectSample",
+      progress: 25,
+      type: "project",
+      hideChildren: false,
+      displayOrder: 1,      
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), 1),
+      end: new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        2,
+        12,
+        28
+      ),
+      name: "Idea",
+      additionalInfo: "Regulation",
+      id: "Task 0",
+      progress: 45,
+      type: "task",
+      project: "ProjectSample",
+      displayOrder: 2,
+      styles: {
+        icon: "R"
       }
     },
     {
-      "id": "45598946227126475",
-      "name": "Duis mollit sunt et do pariatur anim.Et aute duis dolore ad nulla sint nulla proident reprehenderit",
-      "additionalInfo": "",
-      "tooltip": "",
-      "start": new Date("2022-08-24T22:00:00.000Z"),
-      "end": new Date("2022-08-25T21:59:59.999Z"),
-      "type": "project",
-      "project": "44754521296994505",
-      "hideChildren": true,
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#B2B2B2",
-        "backgroundSelectedColor": "#989898",
-        "progressColor": "#B2B2B2",
-        "progressSelectedColor": "#989898"
+      start: new Date(now.getFullYear(), now.getMonth(), 2),
+      end: new Date(now.getFullYear(), now.getMonth(), 4, 0, 0),
+      name: "Research",
+      id: "Task 1",
+      progress: 25,
+      dependencies: ["Task 0"],
+      type: "task",
+      project: "ProjectSample",
+      displayOrder: 3,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), 4),
+      end: new Date(now.getFullYear(), now.getMonth(), 8, 0, 0),
+      name: "Discussion with team",
+      id: "Task 2",
+      progress: 10,
+      dependencies: ["Task 1"],
+      type: "task",
+      project: "ProjectSample",
+      displayOrder: 4,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), 8),
+      end: new Date(now.getFullYear(), now.getMonth(), 9, 0, 0),
+      name: "Developing",
+      id: "Task 3",
+      progress: 2,
+      dependencies: ["Task 2"],
+      type: "task",
+      project: "ProjectSample",
+      displayOrder: 5,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), 8),
+      end: new Date(now.getFullYear(), now.getMonth(), 10),
+      name: "Review",
+      id: "Task 4",
+      type: "task",
+      progress: 70,
+      dependencies: ["Task 2"],
+      project: "ProjectSample",
+      displayOrder: 6,
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), 15),
+      end: new Date(now.getFullYear(), now.getMonth(), 15),
+      name: "Release",
+      id: "Task 6",
+      progress: now.getMonth(),
+      type: "milestone",
+      dependencies: ["Task 4"],
+      project: "ProjectSample",
+      displayOrder: 7,
+    },
+    {
+      start: startOfDay(new Date(now.getFullYear(), now.getMonth(), 18)),
+      end: endOfDay(new Date(now.getFullYear(), now.getMonth(), 18)),
+      name: "Party Project",
+      id: "partyProj",
+      progress: 0,
+      type: "project",
+      hideChildren: false      
+    },
+    {
+      start: new Date(now.getFullYear(), now.getMonth(), 18),
+      end: new Date(now.getFullYear(), now.getMonth(), 19),
+      name: "Party Time",
+      id: "Task 9",
+      progress: 0,
+      type: "task",
+      project: "partyProj",
+      styles: {
+        icon: "O"
       }
     },
     {
-      "id": "22517998136853181",
-      "name": "Id duis sint voluptate pariatur tempor nostrud ipsum excepteur minim",
-      "additionalInfo": "Nisi amet in deserunt ut consectetur enim enim",
-      "tooltip": "",
-      "start": new Date("2022-08-25T10:00:00.000Z"),
-      "end": new Date("2022-08-25T10:00:00.000Z"),
-      "type": "task",
-      "project": "45598946227126475",
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#D85604",
-        "backgroundSelectedColor": "#973C03",
-        "progressColor": "#D85604",
-        "progressSelectedColor": "#973C03",
-        "icon": "♦"
-      }
-    },
-    {
-      "id": "45598946227126373",
-      "name": "Minim quis ad sunt dolore",
-      "additionalInfo": "",
-      "tooltip": "",
-      "start": new Date("2022-08-26T22:00:00.000Z"),
-      "end": new Date("2022-09-22T21:59:59.999Z"),
-      "type": "project",
-      "project": "44754521296994505",
-      "hideChildren": true,
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#B2B2B2",
-        "backgroundSelectedColor": "#989898",
-        "progressColor": "#B2B2B2",
-        "progressSelectedColor": "#989898"
-      }
-    },
-    {
-      "id": "22517998136853681",
-      "name": "123",
-      "additionalInfo": "Commodo eu amet eiusmod occaecat nisi",
-      "tooltip": "",
-      "start": new Date("2022-08-31T10:00:00.000Z"),
-      "end": new Date("2022-08-31T10:00:00.000Z"),
-      "type": "task",
-      "project": "45598946227126373",
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#D85604",
-        "backgroundSelectedColor": "#973C03",
-        "progressColor": "#D85604",
-        "progressSelectedColor": "#973C03",
-        "icon": "♢"
-      }
-    },
-    {
-      "id": "22517998136853682",
-      "name": "333",
-      "additionalInfo": "Duis est do laborum ut sunt voluptate sint",
-      "tooltip": "",
-      "start": new Date("2022-09-22T10:00:00.000Z"),
-      "end": new Date("2022-09-22T10:00:00.000Z"),
-      "type": "task",
-      "project": "45598946227126373",
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#D85604",
-        "backgroundSelectedColor": "#973C03",
-        "progressColor": "#D85604",
-        "progressSelectedColor": "#973C03",
-        "icon": "♢"
-      }
-    },
-    {
-      "id": "22517998136853688",
-      "name": "Quis laboris excepteur eu nulla dolor dolore nostrud exercitation.Ad eiusmod nisi ullamco labore pariatur velit tempor Lorem commodo sunt id.",
-      "additionalInfo": "Voluptate non esse ipsum exercitation",
-      "tooltip": "",
-      "start": new Date("2022-08-27T10:00:00.000Z"),
-      "end": new Date("2022-08-27T10:00:00.000Z"),
-      "type": "task",
-      "project": "45598946227126373",
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#D85604",
-        "backgroundSelectedColor": "#973C03",
-        "progressColor": "#D85604",
-        "progressSelectedColor": "#973C03",
-        "icon": "♦"
-      }
-    },
-    {
-      "id": "44754521296994705",
-      "name": "Deserunt incididunt proident eiusmod culpa nisi anim consectetur labore.Ad esse qui nostrud veniam aliqua deserunt ullamco aliqua.",
-      "additionalInfo": "Enim esse pariatu",
-      "tooltip": "Sit irure fugiat consequat cupidatat nostrud ex veniam officia non cupidatat ut sint.Esse aute veniam magna quis qui ex magna dolor.Laboris anim commodo duis cupidatat aute voluptate sint exercitation reprehenderit do.",
-      "start": new Date("2019-09-26T22:00:00.000Z"),
-      "end": new Date("2022-08-14T21:59:59.999Z"),
-      "type": "project",
-      "hideChildren": true,
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#F8D87D",
-        "backgroundSelectedColor": "#F4C53C",
-        "progressColor": "#F8D87D",
-        "progressSelectedColor": "#F4C53C"
-      }
-    },
-    {
-      "id": "45598946227126673",
-      "name": "G1",
-      "additionalInfo": "",
-      "tooltip": "",
-      "start": new Date("2022-08-13T22:00:00.000Z"),
-      "end": new Date("2022-08-14T21:59:59.999Z"),
-      "type": "project",
-      "project": "44754521296994705",
-      "hideChildren": true,
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#B2B2B2",
-        "backgroundSelectedColor": "#989898",
-        "progressColor": "#B2B2B2",
-        "progressSelectedColor": "#989898"
-      }
-    },
-    {
-      "id": "22517998136854981",
-      "name": "Adipisicing id non occaecat ex deserunt culpa ut incididunt reprehenderit pariatur tempor velit occaecat",
-      "additionalInfo": "Elit exercitation aliquip et tempor",
-      "tooltip": "",
-      "start": new Date("2022-08-14T10:00:00.000Z"),
-      "end": new Date("2022-08-14T10:00:00.000Z"),
-      "type": "task",
-      "project": "45598946227126673",
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#D85604",
-        "backgroundSelectedColor": "#973C03",
-        "progressColor": "#D85604",
-        "progressSelectedColor": "#973C03",
-        "icon": "♢"
-      }
-    },
-    {
-      "id": "45598946227126674",
-      "name": "g2",
-      "additionalInfo": "",
-      "tooltip": "",
-      "start": new Date("2019-09-26T22:00:00.000Z"),
-      "end": new Date("2019-09-27T21:59:59.999Z"),
-      "type": "project",
-      "project": "44754521296994705",
-      "hideChildren": true,
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#B2B2B2",
-        "backgroundSelectedColor": "#989898",
-        "progressColor": "#B2B2B2",
-        "progressSelectedColor": "#989898"
-      }
-    },
-    {
-      "id": "22517998136853411",
-      "name": "2 title 2",
-      "additionalInfo": "Qui reprehenderit id cupidatat sit in esse ex.",
-      "tooltip": "",
-      "start": new Date("2019-09-27T10:00:00.000Z"),
-      "end": new Date("2019-09-27T10:00:00.000Z"),
-      "type": "task",
-      "project": "45598946227126674",
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#D85604",
-        "backgroundSelectedColor": "#973C03",
-        "progressColor": "#D85604",
-        "progressSelectedColor": "#973C03",
-        "icon": "♢"
-      }
-    },
-    {
-      "id": "45598946227126675",
-      "name": "g3",
-      "additionalInfo": "",
-      "tooltip": "",
-      "start": new Date("2022-04-11T22:00:00.000Z"),
-      "end": new Date("2022-04-12T21:59:59.999Z"),
-      "type": "project",
-      "project": "44754521296994705",
-      "hideChildren": true,
-      "hideBarTask": true,
-      "progress": 100,
-      "styles": {
-        "backgroundColor": "#B2B2B2",
-        "backgroundSelectedColor": "#989898",
-        "progressColor": "#B2B2B2",
-        "progressSelectedColor": "#989898"
-      }
-    }
+      start: new Date(now.getFullYear()-1, now.getMonth(), 18),
+      end: new Date(now.getFullYear()-1, now.getMonth(), 19),
+      name: "Disabled task (bar not visible)",
+      id: "Task 10",
+      progress: 0,
+      isDisabled: true,
+      type: "task",
+      hideBarTask: true
+    },    
   ];
   return tasks;
 }
